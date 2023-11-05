4ee7ef5b0331acf1d8d735a8f98884ca7ac47171
Vincentchien1995 <sheepjessie@gmail.com>
2023-11-05 14:53:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def simple_search(tab, item):
+    i = 0
+    while i < len(tab):
+        if tab[i] == item:
+            return i
+        i += 1
+    return -1
+
+nitems = int(input("Enter the number of items: "))
+items = []
+i = 0
+while i < nitems:
+    item = float(input("Enter the item: "))
+    items.append(item)
+    i += 1
+
+print(simple_search(items,4))
